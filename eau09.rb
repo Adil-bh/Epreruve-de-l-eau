9ce62d7d245555becbca9ecc60267f42cc157a88
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-06-07 12:47:29 +0200

    Entre min et max

--- eau09.rb
+++ eau09.rb
@@ -3,6 +3,10 @@ min = nil
 max = nil
 array_min_max = []
 
+def check_arguments_error(arguments)
+	return true if arguments.length != 2 || arguments[0].to_s.count("a-zA-Z") > 0 || arguments[1].to_s.count("a-zA-Z") > 0
+end
+
 def check_min_max(argument1,argument2)
 	if argument1 > argument2
 		max = argument1
@@ -15,17 +19,12 @@ def check_min_max(argument1,argument2)
 	end
 end
 
-def check_arguments_error(arguments)
-	return true if arguments.length != 2 || arguments[0].to_s.count("a-zA-Z") > 0 || arguments[1].to_s.count("a-zA-Z") > 0
-end
-
 if check_arguments_error(args)
 	puts "error"
 else
 	array_min_max = check_min_max(args[0],args[1])
 	min = array_min_max[0].to_i
 	max = array_min_max[1].to_i
-
 	for num in 0..max
 		if num >= min && num < max
 			print "#{num} "
