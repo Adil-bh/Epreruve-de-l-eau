e2728342b3b18cbbf95a8c9a0db75f62c12be142
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-07-04 23:52:20 +0200

    Prochain nombre premier

--- eau04.rb
+++ eau04.rb
@@ -1,9 +1,8 @@
 =begin	
 Pourquoi est-ce que je ne peux pas utiliser dans ma méthode, une variable que j'ai déclaré en dehors ?	
 =end
-arguments = ARGV
-number_arg = ARGV[0].to_f
 
+#Fonction utilisées
 def isPrime(val)
 	number = val
 	divider_array = []
@@ -16,14 +15,21 @@ def isPrime(val)
 	return true if divider_array.length == 2
 end
 
-if number_arg == nil || arguments.length != 1 || number_arg.to_s.count("a-zA-Z") > 0 || number_arg <= 0 || number_arg == 1
+#Partie 1 : Gestion d'erreur
+#Partie 2 : Parsing
+arguments = ARGV
+number_arg = ARGV[0].to_f
+if number_arg == nil || arguments.length != 1 || number_arg.to_s.count("a-zA-Z") > 0 || number_arg <= 0
 	puts "-1"
-else
-	loop do
-		number_arg = number_arg + 1
-		if isPrime(number_arg)
-			puts "#{number_arg.to_i}"
-			break
-		end
+	exit
+end
+
+#Partie 3 : Résolution
+#Partie 4 : Affichage
+loop do
+	number_arg = number_arg + 1
+	if isPrime(number_arg)
+		puts "#{number_arg.to_i}"
+		break
 	end
 end
