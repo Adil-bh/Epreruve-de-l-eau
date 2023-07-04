eed80d4ccc11a2fd583f9e71f1cdccac95361329
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-07-04 23:58:00 +0200

    Chiffres only

--- eau08.rb
+++ eau08.rb
@@ -1,7 +1,4 @@
-args = ARGV
-sequence_of_chars = ARGV[0].to_s.chars
-number_count = 0
-
+#Fonction utilisées
 def is_Number?(ascii)
 	return true if ascii.between?(48,57)
 	return false if !ascii.between?(48,57)
@@ -12,17 +9,41 @@ def check_Numbers(count,array)
 	return false if count != array.length
 end
 
-if args.length != 1 #Vérifier si certains caractères sont des caractères spéciaux ?
-	puts "error"
-else
-	sequence_of_chars.each do |char|
-		number_ascii = char.ord
-		if !is_Number?(number_ascii)
-			next
-		elsif is_Number?(number_ascii)
-			number_count += 1
-		end
+def contains_special_character?(argument)
+	regex = /[!@#$%^&*(),.?":{}|<>]/
+	if argument =~ regex 
+		return true 
+	else 
+		return false
 	end
 end
 
+#Partie 1 : Gestion d'erreur
+if ARGV.length != 1 
+	puts "error"
+	exit
+end
+
+ARGV.each do |argument|
+	if contains_special_character?(argument)
+		puts "error : non-allowed character"
+		exit
+	end
+end
+
+#Partie 2 : Parsing
+sequence_of_chars = ARGV[0].to_s.chars
+number_count = 0
+
+#Partie 3 : Résolution
+sequence_of_chars.each do |char|
+	number_ascii = char.ord
+	if !is_Number?(number_ascii)
+		next
+	elsif is_Number?(number_ascii)
+		number_count += 1
+	end
+end
+
+#Partie 4 : Affichage
 puts check_Numbers(number_count,sequence_of_chars)
