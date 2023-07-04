acb677be06d5d4d90a00bfb15eb880f5bc9a6b42
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-07-04 23:53:30 +0200

    Majuscule sur deux

--- eau06.rb
+++ eau06.rb
@@ -1,8 +1,4 @@
-args = ARGV
-input_string = ARGV[0].to_s.chars #On découpe le mot en caractères dans un array
-output_string = []
-count = 0
-
+#Fonction utilisées
 def is_even?(number)
 	return true if number % 2 == 0
 	return false if number % 2 != 0
@@ -22,26 +18,34 @@ def display_output(array_ascii)
 	return output.join
 end
 
-if args.length != 1 || args[0].to_s.count("0-9") > 0
+#Partie 1 : Gestion d'erreur
+if ARGV.length != 1 || ARGV[0].to_s.count("0-9") > 0
 	puts "error"
-else
-	input_string.each do |char| 						#On passe en code ASCII chaque caractère pour agir sur sa valeur
-		letter = char.ord
-			if is_even?(count) && is_upper?(letter) 	#Si le compteur est pair la lettre sera majuscule 
-				output_string.insert(-1, letter)
-			elsif is_even?(count) && !is_upper?(letter) #Si le compteur est pair et la lettre est min. on passe son code ASCII au code MAJ.
-				letter -= 32
-				output_string.insert(-1, letter)
-			elsif letter == 32 							#Pour éviter les espaces
-				count -= 1
-				output_string.insert(-1, letter)
-			else
-				output_string.insert(-1, letter)
-			end
-		count += 1
-	end
-	print display_output(output_string)
+	exit
 end
 
+#Partie 2 : Parsing
+input_string = ARGV[0].to_s.chars #On découpe le mot en caractères dans un array
+output_string = []
+count = 0
 
+#Partie 3 : Résolution
+input_string.each do |char| 						#On passe en code ASCII chaque caractère pour agir sur sa valeur
+	letter = char.ord
+		if is_even?(count) && is_upper?(letter) 	#Si le compteur est pair la lettre sera majuscule 
+			output_string.insert(-1, letter)
+		elsif is_even?(count) && !is_upper?(letter) #Si le compteur est pair et la lettre est min. on passe son code ASCII au code MAJ.
+			letter -= 32
+			output_string.insert(-1, letter)
+		elsif letter == 32 							#Pour éviter les espaces
+			count -= 1
+			output_string.insert(-1, letter)
+		else
+			output_string.insert(-1, letter)
+		end
+	count += 1
+end
+
+#Partie 4 : Affichage
+print display_output(output_string)
 
