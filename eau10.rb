788133a037b4f924b810b3cb1a7fa70526e67525
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-07-05 00:00:00 +0200

    Index wanted

--- eau10.rb
+++ eau10.rb
@@ -1,8 +1,4 @@
-args = ARGV
-wanted_word = args[-1]
-word_count = 0
-wanted_word_index = nil
-
+#Fonction utilisées
 def check_arguments(arguments) #Vérifier aussi les caracrtères spéciaux ?
 	return true if arguments.length < 2
 end
@@ -17,20 +13,45 @@ def contains_wanted_word(word, array)
 	end
 end
 
-if check_arguments(args)
-	puts "error"
-else
-	args.pop() #On supprime le dernier mot du tableau pour pouvoir parser
-	if contains_wanted_word(wanted_word,args) != true #On vérifie si le mot est bien dans le tableau des arguments
-		puts "-1"
-	elsif contains_wanted_word(wanted_word,args) == true #Si oui on compare le mot cherché à chaque valeur dans le tableau
-		args.each do |word|
-			if word == wanted_word
-				wanted_word_index = word_count
-				break
-			end
-			word_count += 1
-		end
-		puts wanted_word_index
+def contains_special_character?(argument)
+	regex = /[!@#$%^&*(),.?":{}|<>]/
+	if argument =~ regex 
+		return true 
+	else 
+		return false
 	end
+end
+
+#Partie 1 : Gestion d'erreur
+if check_arguments(ARGV)
+	puts "error"
+	exit
+end
+
+ARGV.each do |argument|
+	if contains_special_character?(argument)
+		puts "error : non-allowed character"
+		exit
+	end
+end
+
+#Partie 2 : Parsing
+wanted_word = ARGV[-1]
+word_count = 0
+wanted_word_index = nil
+
+#Partie 3 : Résolution
+#Partie 4 : Affichage
+ARGV.pop() 				#On supprime le dernier mot du tableau pour pouvoir parser
+if contains_wanted_word(wanted_word,ARGV) != true #On vérifie si le mot est bien dans le tableau des arguments
+	puts "-1"
+elsif contains_wanted_word(wanted_word,ARGV) == true #Si oui on compare le mot cherché à chaque valeur dans le tableau
+	ARGV.each do |word|
+		if word == wanted_word
+			wanted_word_index = word_count
+			break
+		end
+		word_count += 1
+	end
+	puts wanted_word_index
 end
