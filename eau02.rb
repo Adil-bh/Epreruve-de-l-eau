bb5a54fba92dd80fef82ddbe58038b3a362a6866
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-07-04 23:51:08 +0200

    Paramètres à l’envers

--- eau02.rb
+++ eau02.rb
@@ -1,11 +1,17 @@
+#Partie 1 : Gestion d'erreur
+if ARGV.length == 0
+	puts "Error."
+	exit
+end
+
+#Partie 2 : Parsing
 arguments = ARGV
 count = ARGV.length - 1
 
-if arguments.length == 0
-	puts "Error."
-else
-	arguments.each do
-		puts arguments[count]
-		count -= 1
-	end
+
+#Partie 3 : Résolution
+#Partie 4 : Affichage
+arguments.each do
+	puts arguments[count]
+	count -= 1
 end
