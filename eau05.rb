46b44969ef3c2b713c779d450bd6d52177913512
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-07-09 19:21:34 +0200

    String dans string : Simplification fonction containsWord

--- eau05.rb
+++ eau05.rb
@@ -1,21 +1,20 @@
 #Fonction utilisées
 def containsWord?(full_word, word)
-	i = 3
-	j = 0
-	letter_count = 0
-	until letter_count == word.length
-		if full_word[i] != word[j]
-			i += 1
-		elsif full_word[i] == word[j]
-			i += 1
-			j += 1
-			letter_count += 1
-		end
-		if i > full_word.length
-			break
-		end
-	end
-	return true if letter_count == word.length
+  i = 0
+  letter_count = 0
+
+  until letter_count == word.length || i >= full_word.length
+    if full_word[i] == word[letter_count]
+      letter_count += 1
+    end
+    i += 1
+  end
+
+  return letter_count == word.length
+end
+
+def checkArguments(arguments)
+	return true if arguments.length != 2 || arguments[0].count("0-9") > 0 || arguments[1].count("0-9") > 0
 end
 
 #Partie 1 : Gestion d'erreur
@@ -23,15 +22,11 @@ end
 arguments = ARGV
 full_word = arguments[0]
 word = arguments[1]
-if arguments.length != 2 || full_word.count("0-9") > 0 || word.count("0-9") > 0
+
+if checkArguments(arguments)
 	puts "error"
 	exit
 end
-
 #Partie 3 : Résolution
 #Partie 4 : Affichage
-if containsWord?(full_word, word) == true
-	puts "True"
-else
-	puts "False"
-end
+puts containsWord?(full_word, word)
